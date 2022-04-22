6dc5264f5631eeef0f302c9e529c2703292dac44
LeonarCD <leonardofelipe931@gmail.com>
2022-04-22 17:10:21 -0400

    feat: SidebarDrawer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 
 import { Logo } from "./Logo";
 import { NotificationNav } from "./NotificationNav";
@@ -6,6 +8,9 @@ import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +26,20 @@ export function Header() {
       px="6"
       align="center"
     >
+      { !isWideVersion && (
+        <IconButton
+          aria-label="Open sidebar" 
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+          paddingTop="1.5"
+        >
+
+        </IconButton>
+      )}
+
       <Logo />
 
       { isWideVersion && <SearchBox />}
