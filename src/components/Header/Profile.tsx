0ba2edf179f7f3971d35360ce4f6edc3e7eb1e59
LeonarCD <leonardofelipe931@gmail.com>
2022-04-22 15:43:52 -0400

    feat: header responsiveness

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Leonardo Carvalho</Text>
-        <Text color="gray.300" fontSize="small">
-          leonardo.carvalho@pigz.com.br
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+            <Text>Leonardo Carvalho</Text>
+            <Text color="gray.300" fontSize="small">
+              leonardo.carvalho@pigz.com.br
+            </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Leo Carvalho" src="https://github.com/LeonardoCD.png" />
     </Flex>
