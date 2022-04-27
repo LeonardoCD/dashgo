fd3d1b2f393dd10de212a97648dec188feca0387
LeonarCD <leonardofelipe931@gmail.com>
2022-04-27 15:49:05 -0400

    feat: init mirage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import { AppProps } from 'next/app'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { makeServer } from '../services/mirage'
 import { theme } from '../styles/theme'
+
+
+if(process.env.NODE_ENV === 'development') {
+  makeServer()
+}
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SidebarDrawerProvider>
-      <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme}>
+      <SidebarDrawerProvider>
       <Component {...pageProps} />
+      </SidebarDrawerProvider>
     </ChakraProvider>
-    </SidebarDrawerProvider>
   )
 }
 
