7bb7006d4f62303bde9d2b51d624f27d16b03c77
LeonarCD <leonardofelipe931@gmail.com>
2022-04-27 16:42:39 -0400

    feat: query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,22 +1,23 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    return data
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(res => res.json())
-      .then(data => console.log(data))
-  }, [])
 
   return (
     <Box>
@@ -40,93 +41,105 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Leonardo Carvalho</Text>
-                    <Text fontSize="sm" color="gray.300">leonardo.carvalho@pigz.com.br</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>04 de Abril, 2022</Td>}
-                <Td>
-                  { isWideVersion && <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    { isWideVersion ? 'Editar' : ''}
-                  </Button>}
-                </Td>
-              </Tr>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    { isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+              <Tbody>
+                <Tr>
+                  <Td px={["4", "4", "6"]}>
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">Leonardo Carvalho</Text>
+                      <Text fontSize="sm" color="gray.300">leonardo.carvalho@pigz.com.br</Text>
+                    </Box>
+                  </Td>
+                  { isWideVersion && <Td>04 de Abril, 2022</Td>}
+                  <Td>
+                    { isWideVersion && <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      { isWideVersion ? 'Editar' : ''}
+                    </Button>}
+                  </Td>
+                </Tr>
+                    
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Leonardo Carvalho</Text>
+                        <Text fontSize="sm" color="gray.300">leonardo.carvalho@pigz.com.br</Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && <Td>04 de Abril, 2022</Td>}
+                    <Td>
+                      { isWideVersion && <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        { isWideVersion ? 'Editar' : ''}
+                      </Button>}
+                    </Td>
+                  </Tr>
+                    
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Leonardo Carvalho</Text>
+                        <Text fontSize="sm" color="gray.300">leonardo.carvalho@pigz.com.br</Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && <Td>04 de Abril, 2022</Td>}
+                    <Td>
+                      { isWideVersion && <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        { isWideVersion ? 'Editar' : ''}
+                      </Button>}
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Leonardo Carvalho</Text>
-                    <Text fontSize="sm" color="gray.300">leonardo.carvalho@pigz.com.br</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>04 de Abril, 2022</Td>}
-                <Td>
-                  { isWideVersion && <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    { isWideVersion ? 'Editar' : ''}
-                  </Button>}
-                </Td>
-              </Tr>
-
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Leonardo Carvalho</Text>
-                    <Text fontSize="sm" color="gray.300">leonardo.carvalho@pigz.com.br</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>04 de Abril, 2022</Td>}
-                <Td>
-                  { isWideVersion && <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    { isWideVersion ? 'Editar' : ''}
-                  </Button>}
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
